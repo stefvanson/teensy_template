5d8448abc63011672eabf8112ee8d5355cf468f2
Stef van Son <stefvanson@gmail.com>
2021-04-27 21:51:07 +0200

    Added ability to generate Doxygen documentation.

--- src/module1/module1.cpp
+++ src/module1/module1.cpp
@@ -1,3 +1,9 @@
+/** @file
+ *  @brief Source file for module1.
+ *  @author Stef van Son
+ *  @date 2021-04-27
+ *  @copyright Stef van Son, 2021
+ */
 #include "module1.hpp"
 
 int module1_function(int return_value) {
--- src/module1/module1.hpp
+++ src/module1/module1.hpp
@@ -1,6 +1,16 @@
+/** @file
+ *  @brief Header file for module1.
+ *  @author Stef van Son
+ *  @date 2021-04-27
+ *  @copyright Stef van Son, 2021
+ */
 #ifndef MODULE1_MODULE1_HPP_
 #define MODULE1_MODULE1_HPP_
 
+/** @brief Dummy function for illustrating the template.
+ *  @param[in] return_value The value that the function returns.
+ *  @returns An integer that is equal to the return_value input parameter.
+ */
 int module1_function(int return_value);
 
 #endif  /* MODULE1_MODULE1_HPP_ */
